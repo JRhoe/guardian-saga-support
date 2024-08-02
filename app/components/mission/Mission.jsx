26f5ddc089f87ac49a74108a20175dba656c8f85
James <jamesrhoe1@gmail.com>
2024-08-01 21:44:42 -0700

    updated mission statement component

--- app/components/mission/Mission.jsx
+++ app/components/mission/Mission.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import styles from "./Mission.module.css"
+import img1 from "../../../public/MissionCara/DSC04264.jpg"
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Mission = () => {
     return (
         <div className={styles.container}>
             <div className={styles.row}>
-                <h1 className={styles.title}>Training the next generation of <br/>Earth's guardians</h1>
-                <p>We are a non-profit dedicated to providing underprivileged youth with empowering programs such as self-defense, 
-                    team building, and the great outdoors.  Our dream is to 
-                    equip the next generation with tools to build a future in which all people—and our Earth—thrive.</p>
+                <div className={styles.statement}>
+                    <h1 className={styles.statementTitle}>Training the next generation of Earth's guardians</h1>
+                    <p className={styles.statementPara}>We are a non-profit dedicated to providing underprivileged youth with empowering programs such as self-defense, 
+                        team building, and the great outdoors.  Our dream is to 
+                        equip the next generation with tools to build a future in which all people—and our Earth—thrive.</p>
+                    <Link 
+                    className={styles.statementLink}
+                    href="/">Learn more about how we help</Link>
+                </div>
+                {/* make this into a separate component */}
+                <div className={styles.imgCara}>
+                    <div className={styles.firstRow}>
+                        <Image
+                        src={img1}
+                        className={styles.missionCaraImg}
+                        />
+                        <Image
+                        src={img1}
+                        className={styles.missionCaraImg}
+                        />
+                    </div>
+                    <div className={styles.secondRow}>
+                        <Image
+                        src={img1}
+                        className={styles.missionCaraImg}
+                        />
+                        <Image
+                        src={img1}
+                        className={styles.missionCaraImg}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
